ab766139e5512b797f1d01a7f51b3ef92142c32d
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2024-11-24 22:01:24 +0200

    task 1 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+const value = 100;
+
+function add(a, b) {
+    return a + b
+}
+
+function sum(a, b) {
+    return a * b + value
+}
+
+export { add, sum }
+
+//const a = 15;
+//const b = 20;
+
+//export { add, sum }
+//export default a;
+
+// Потрібно створити функцію яка буде формувати чергу в магазині
+//1 перший параметр масив, кількість елементів це кількість покупців,
+//значення елементу масиву це час який покупець витратить на касі.
+//2 параметр це кількість кас
+//Повертає об'єкт де ключ це номер каси, значення це час черги на кассі
+//12 + 2 + 15 = 29
+//3 + 5 + 6 + 7 = 21
+//{ 1 : 29, 2 : 21}
+
+function fn(arr, cash) {
+    const obj = {}
+
+    for (let i = 1; i <= cash; i += 1) {
+        obj[i] = 0
+    }
+    console.log(obj);
+    arr.forEach(bayer => {
+        const values = Object.values(obj)
+        const minValue = Math.min(...values)
+        const currentCase = values.indexOf(minValue) + 1
+        obj[currentCase] += bayer
+    })
+    return obj
+}
+
+
+console.log(fn([12, 3, 5, 6, 2, 15, 7], 2));
+
 
