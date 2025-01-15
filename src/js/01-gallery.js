60ac086288b57e4b84a61d2cd5e7830cef8ff6bb
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2025-01-15 23:55:07 +0200

    update video.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,12 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import {sum as fnSum, add} from './02-video'
+import * as data from './02-video'
+//const sum = 10
+//console.log(galleryItems);
+//console.log(sum);
+//console.log(fnSum(4,5));
+//console.log(add(2,2));
+console.log(data.add(2,3));
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+const value = 100
+
+
+function add(a, b) {
+    return a + b
+}
+
+function sum(a, b) {
+    return a * b + value
+}
+
+
+export {add, sum}
