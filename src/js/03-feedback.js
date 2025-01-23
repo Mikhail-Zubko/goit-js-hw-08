ae4022064dbcc192a32469c7739e75ac0f476d3b
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2025-01-23 23:37:20 +0200

    saved form to localstorage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,58 @@
+import { throttle } from 'lodash';
 
+const refs = {
+    form: document.querySelector('.js-feedback-form'),
+    emailarea: document.querySelector('.js-feedback-form input'),
+    textarea: document.querySelector('.js-feedback-form textarea')
+}
+
+refs.form.addEventListener('submit', onFormSubmit)
+refs.emailarea.addEventListener('input', onEmailareaInput)
+refs.textarea.addEventListener('input', onTextareaInput)
+
+populateEmailarea()
+populateTextarea()
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault()
+
+    console.log('отправляем форму');
+
+    evt.currentTarget.reset()
+}
+
+function onEmailareaInput(evt) {
+    const email = evt.currentTarget.value
+
+    localStorage.setItem('feedback-eml', email)
+}
+
+function onTextareaInput(evt) {
+    const message = evt.currentTarget.value
+
+    localStorage.setItem('feedback-msg', message)
+}
+
+
+function populateEmailarea() {
+    const savedEmail = localStorage.getItem('feedback-eml')
+
+    if (savedEmail) {
+        console.log(savedEmail);
+        refs.emailarea.value = savedEmail
+    }
+}
+
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem('feedback-msg')
+
+    if (savedMessage) {
+        console.log(savedMessage);
+        refs.textarea.value = savedMessage
+
+    }
+
+
+}
