2d6b9a1b596c5f5f418e687a06b56b69778238cf
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2025-01-27 00:46:51 +0200

    update time

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,29 @@
 import { throttle } from 'lodash';
 
+const STORAGE_KEY_EMAIL = 'feedback-eml';
+const STORAGE_KEY_MESSAGE = 'feedback-msg';
+
+const formData = {};
+
 const refs = {
     form: document.querySelector('.js-feedback-form'),
-    emailarea: document.querySelector('.js-feedback-form input'),
-    textarea: document.querySelector('.js-feedback-form textarea')
+    //emailarea: document.querySelector('.js-feedback-form input'),
+    //textarea: document.querySelector('.js-feedback-form textarea')
 }
 
 refs.form.addEventListener('submit', onFormSubmit)
-refs.emailarea.addEventListener('input', onEmailareaInput)
-refs.textarea.addEventListener('input', onTextareaInput)
+//refs.emailarea.addEventListener('input', throttle(onEmailareaInput, 500))
+// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500))
+
+refs.form.addEventListener('input', e => {
+    //console.log(e.target.name);
+    //console.log(e.target.value);
+
+    formData[e.target.name] = e.target.value
+
+    console.log(formData);
+    // дальше нужно сохранить в локал сторедж
+})
 
 populateEmailarea()
 populateTextarea()
@@ -20,23 +35,29 @@ function onFormSubmit(evt) {
     console.log('отправляем форму');
 
     evt.currentTarget.reset()
+    localStorage.removeItem(STORAGE_KEY_EMAIL)
+    localStorage.removeItem(STORAGE_KEY_MESSAGE)
 }
 
-function onEmailareaInput(evt) {
-    const email = evt.currentTarget.value
-
-    localStorage.setItem('feedback-eml', email)
+function formData(evt) {
+    const email = evt.target.value
+    localStorage.setItem(STORAGE_KEY_EMAIL, email)
 }
+//function onEmailareaInput(evt) {
+//const email = evt.target.value
 
-function onTextareaInput(evt) {
-    const message = evt.currentTarget.value
+// localStorage.setItem(STORAGE_KEY_EMAIL, email)
+//}
 
-    localStorage.setItem('feedback-msg', message)
-}
+//function onTextareaInput(evt) {
+// const message = evt.target.value
+
+//  localStorage.setItem(STORAGE_KEY_MESSAGE, message)
+//}
 
 
 function populateEmailarea() {
-    const savedEmail = localStorage.getItem('feedback-eml')
+    const savedEmail = localStorage.getItem(STORAGE_KEY_EMAIL)
 
     if (savedEmail) {
         console.log(savedEmail);
@@ -46,7 +67,7 @@ function populateEmailarea() {
 
 
 function populateTextarea() {
-    const savedMessage = localStorage.getItem('feedback-msg')
+    const savedMessage = localStorage.getItem(STORAGE_KEY_MESSAGE)
 
     if (savedMessage) {
         console.log(savedMessage);
